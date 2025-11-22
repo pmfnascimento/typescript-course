39658d92777e879921e144185ab964746ac60bed
Nascimento Pedro <pedro.nascimento@natixis.com>
2025-11-22 00:25:08 +0000

    arrays and objects

---
+++ 02 - essentials/arrays-objects.js
@@ -0,0 +1,3 @@
+var hobbies = ['Sports', 'Cooking'];
+hobbies.push(10);
+hobbies.forEach(function (a) { return console.log(a); });
---
+++ 02 - essentials/arrays-objects.ts
@@ -0,0 +1,12 @@
+let hobbies: any[] = ['Sports', 'Cooking'];
+
+hobbies.push(10);
+
+hobbies.forEach(a => console.log(a));
+
+let users: Array<string | number>;
+
+users = ['10',5];
+users = [5, 1];
+users = ['Max', 'Ana'];
+
