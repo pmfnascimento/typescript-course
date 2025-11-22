b9fff07aa49cd5ebf5c573f40bfaae4e735e9510
Nascimento Pedro <pedro.nascimento@natixis.com>
2025-11-22 12:34:42 +0000

    The "unknown" Type

---
+++ 02 - essentials/unknown.ts
@@ -0,0 +1,11 @@
+function process(val: unknown) {
+
+    if (
+        typeof val === 'object' &&
+        !!val &&
+        'log' in val &&
+        typeof val.log === 'function'
+    ) {
+        val.log();
+    }
+}
