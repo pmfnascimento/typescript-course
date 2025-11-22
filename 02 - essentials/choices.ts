cff00cedba3f81388d04861ba2d268c6a7ee361a
Nascimento Pedro <pedro.nascimento@natixis.com>
2025-11-22 11:54:24 +0000

    Type Aliases & Custom Types

--- 02 - essentials/choices.ts
+++ 02 - essentials/choices.ts
@@ -4,12 +4,25 @@
 //     Guest = 'Guest'
 // }
 
-let userRole: 'admin' | 'editor' | 'guest' = 'admin';
+type Role = 'admin' | 'editor' | 'guest' | 'reader';
+
+type User = {
+    name: string,
+    age: number,
+    role: Role,
+    permitions: string[]
+}
+
+let userRole: Role = 'admin';
 
 userRole = 'admin';
 
 let possibleResults: [1 | -1, 1 | -1];
 
 possibleResults = [1, -1]; 
+
+function access(role: Role) {
+
+}
 
 
