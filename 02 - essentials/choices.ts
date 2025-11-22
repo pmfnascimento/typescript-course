eaaf3148a30e0d42a4aa580c59f3b5c99d6728d8
Nascimento Pedro <pedro.nascimento@natixis.com>
2025-11-22 11:51:09 +0000

    Being Specific With Literal Types

---
+++ 02 - essentials/choices.ts
@@ -0,0 +1,15 @@
+// enum Role {
+//     Admin = 'Admin',
+//     Editor = 'Editor',
+//     Guest = 'Guest'
+// }
+
+let userRole: 'admin' | 'editor' | 'guest' = 'admin';
+
+userRole = 'admin';
+
+let possibleResults: [1 | -1, 1 | -1];
+
+possibleResults = [1, -1]; 
+
+
