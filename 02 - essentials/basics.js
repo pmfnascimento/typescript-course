25df92eb9b3360981c40106b13068c509903d734
Nascimento Pedro <pedro.nascimento@natixis.com>
2025-11-22 00:26:18 +0000

    The basics

---
+++ 02 - essentials/basics.js
@@ -0,0 +1,9 @@
+var userName; // number, boolean
+var userAge = 38;
+userName = 'Max';
+// userAge = '34';
+function add(a, b) {
+    if (b === void 0) { b = 5; }
+    return a + b;
+}
+console.log(add(5));
---
+++ 02 - essentials/basics.ts
@@ -0,0 +1,15 @@
+let userName: string; // number, boolean
+let userAge = 38;
+
+userName = 'Max';
+// userAge = '34';
+
+function add(a:number,b = 5) {
+    return a + b;
+}
+
+ 
+
+add(10);
+// add('10');
+add(10, 6);
