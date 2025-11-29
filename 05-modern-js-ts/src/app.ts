4dc3c6b2ac395cacd84739e41815491a7ec74940
Pedro Nascimento <pmfnascimento@gmail.com>
2025-11-29 23:24:08 +0000

    Rest Parameters

--- 05-modern-js-ts/src/app.ts
+++ 05-modern-js-ts/src/app.ts
@@ -18,26 +18,26 @@ var result;
 
 // console.log(isOld);
 
-const add = (a: number, b: number = 1) => a + b;
+// const add = (a: number, b: number = 1) => a + b;
 
-console.log(add(2, 5));
+// console.log(add(2, 5));
 
-const printOutput: (a: number | string) => void = output => console.log(output);
+// const printOutput: (a: number | string) => void = output => console.log(output);
 
-const button = document.querySelector('button');
+// const button = document.querySelector('button');
 
-if (button) {
-    button.addEventListener('click', event => console.log(event));
-}
+// if (button) {
+//     button.addEventListener('click', event => console.log(event));
+// }
 
-console.log(add(2));
+// console.log(add(2));
 
 const hobbies: string[] = ['Sports', 'Cooking'];
 const activeHobbies: string[] = ['Hiking'];
 
 activeHobbies.push(...hobbies);
 
-console.log(activeHobbies);
+// console.log(activeHobbies);
 
 const person = {
     name: 'Max',
@@ -46,5 +46,15 @@ const person = {
 
 const copiePerson = { ...person };
 
-console.log(copiePerson);
+const add = (...numbers: number[]) => {
+
+    return numbers.reduce((currentResult, currentValue) => {
+        return currentResult + currentValue;
+    }, 0);
+};
+
+const addedNumbers = add(2, 4, 6, 8.5, 1.3);
+console.log(addedNumbers);
+
+
 
