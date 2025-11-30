e577f835583a344a474c7b7124da810187898865
Pedro Nascimento <pmfnascimento@gmail.com>
2025-11-30 18:41:36 +0000

    Working with Function Overloads

--- 07-advanced-types/functions.ts
+++ 07-advanced-types/functions.ts
@@ -1,3 +1,5 @@
+function getLenght(val: any[]): number;
+function getLenght(val: string): string;
 function getLenght(val: string | any[]) {
   if (typeof val === "string") {
     const numbersOfWords = val.split(" ").length;
