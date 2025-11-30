bb675067a3596b319bc945c780bf0e4c1f5e15cd
Pedro Nascimento <pmfnascimento@gmail.com>
2025-11-30 11:45:57 +0000

    A Useful TypeScript Shortcut & Compiling to JavaScript

--- 06-classes-interfaces/basics.js
+++ 06-classes-interfaces/basics.js
@@ -1,3 +1,11 @@
+// class User {
+//     name: string;
+//     age: number;
+//     constructor(name: string, age: number) {
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
 var User = /** @class */ (function () {
     function User(name, age) {
         this.name = name;
@@ -5,4 +13,6 @@ var User = /** @class */ (function () {
     }
     return User;
 }());
-console.log(new User('John', 23));
+var max = new User('Max', 23);
+var fred = new User('Fred', 23);
+console.log(max, fred);
--- 06-classes-interfaces/basics.ts
+++ 06-classes-interfaces/basics.ts
@@ -1,13 +1,19 @@
+// class User {
+
+//     name: string;
+//     age: number;
+
+//     constructor(name: string, age: number) {
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+
 class User {
-
-    name: string;
-    age: number;
-
-    constructor(name: string, age: number) {
-        this.name = name;
-        this.age = age;
-    }
+    constructor(public name: string, public age: number) { }
 }
 
-console.log(new User('John', 23));
+const max = new User('Max', 23);
+const fred = new User('Fred', 23);
+console.log(max, fred);
 
