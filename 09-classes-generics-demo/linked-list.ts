f842f8a110263f2026a5fb9e29e80b1a496dea2e
Nascimento Pedro <pedro.nascimento@natixis.com>
2025-11-30 21:13:07 +0000

    Accessing the Data & Compiling + Running the Code

--- 09-classes-generics-demo/linked-list.ts
+++ 09-classes-generics-demo/linked-list.ts
@@ -20,7 +20,26 @@ class LinkedList<T> {
     }
     this.lenght++;
   }
+
+  getNumberOfElements() {
+    return this.lenght;
+  }
+
+  print() {
+    let current = this.root;
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
 }
 
 const numberList = new LinkedList<number>();
+
+numberList.add(10);
+numberList.add(50);
+numberList.add(-3);
+console.log(numberList.getNumberOfElements());
+numberList.print();
+
 const nameList = new LinkedList<string>();
