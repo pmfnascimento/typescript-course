a55bd7f072a2713060e5cde9eccded8bb489101e
Nascimento Pedro <pedro.nascimento@natixis.com>
2025-11-30 21:08:40 +0000

    Adding Items More Efficiently

--- 09-classes-generics-demo/linked-list.ts
+++ 09-classes-generics-demo/linked-list.ts
@@ -6,18 +6,17 @@ class ListNode<T> {
 
 class LinkedList<T> {
   private root?: ListNode<T>;
+  private tail?: ListNode<T>;
   private lenght = 0;
 
   add(value: T) {
     const node = new ListNode(value);
-    if(!this.root) {
-        this.root = node;
+    if (!this.root || !this.tail) {
+      this.root = node;
+      this.tail = node;
     } else {
-        let current = this.root;
-        while(current.next) {
-            current = current.next;
-        }
-        current.next = node;
+      this.tail.next = node;
+      this.tail = node;
     }
     this.lenght++;
   }
