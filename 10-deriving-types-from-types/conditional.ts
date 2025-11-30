b53abfa976c83633c802f9575411a2f3f64c15c5
Nascimento Pedro <pedro.nascimento@natixis.com>
2025-11-30 22:47:52 +0000

    Introducing Conditional Types

---
+++ 10-deriving-types-from-types/conditional.ts
@@ -0,0 +1,17 @@
+type StringArray = string[];
+// type ElementType<T extends any[]> = T[number];
+
+// type Example1 = ElementType<StringArray>
+
+// let text = "Hello, World!";
+
+let text = 1;
+
+// type Example2 = ElementType<typeof text>
+
+type GetElementType<T> = T extends any[] ? T[number] : never;
+
+type Example1 = GetElementType<StringArray>;
+type Example2 = GetElementType<typeof text>
+
+
