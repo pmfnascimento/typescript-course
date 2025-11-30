d6f9dc665e114837f450623d2b52e87fe3edbebe
Pedro Nascimento <pmfnascimento@gmail.com>
2025-11-30 18:47:32 +0000

    Making Sense of Index Types

---
+++ 07-advanced-types/advanced.js
@@ -0,0 +1,13 @@
+var store = {};
+store.id = 5;
+store.isOpen = true;
+store.name = "Max";
+store.hobbies = ["Sports", "Cooking"];
+store.address = {
+    street: "Main Street",
+    city: "New York",
+    state: "NY",
+    zip: "10001",
+    country: "USA",
+};
+console.log(store);
---
+++ 07-advanced-types/advanced.ts
@@ -0,0 +1,19 @@
+type DataStore = {
+  [prop: string]: string[] | number | string | boolean | Object;
+};
+
+const store: DataStore = {};
+
+store.id = 5;
+store.isOpen = true;
+store.name = "Max";
+store.hobbies = ["Sports", "Cooking"];
+store.address = {
+  street: "Main Street",
+  city: "New York",
+  state: "NY",
+  zip: "10001",
+  country: "USA",
+};
+
+console.log(store);
