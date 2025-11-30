ffd0a9b32948ad47f7962505e2145419b9ddfa7f
Pedro Nascimento <pmfnascimento@gmail.com>
2025-11-30 18:34:24 +0000

    "Outsourcing" Type Guards & Using Type Predicates

--- guards.ts
+++ guards.ts
@@ -1,29 +1,37 @@
-type FileSource = { type: 'file', path: string };
+type FileSource = { type: "file"; path: string };
 const fileSource: FileSource = {
   path: "some/path/to/file.csv",
-  type: 'file'
+  type: "file",
 };
 
-type DBSource = { type: 'db', connectionUrl: string };
+type DBSource = { type: "db"; connectionUrl: string };
 const dbSource: DBSource = {
   connectionUrl: "some-connection-url",
-  type: 'db'
+  type: "db",
 };
 
 type Source = FileSource | DBSource;
 
-function loadData(source: Source) {
-    // Open + read file OR reach out to database server
-    // if ("path" in source) {
-    //   return;
-    // }
-
-    if(source.type === 'file') {
-        console.log(source.path);
-        return;
-    }
-
-    console.log(source.connectionUrl);
-    return; 
+function isFile(source: Source) {
+  return source.type === "file";
 }
 
+function loadData(source: Source) {
+  // Open + read file OR reach out to database server
+  // if ("path" in source) {
+  //   return;
+  // }
+
+  // if (source.type === "file") {
+  //   console.log(source.path);
+  //   return;
+  // }
+
+  if (isFile(source)) {
+    console.log(source.path);
+    return;
+  }
+
+  console.log(source.connectionUrl);
+  return;
+}
