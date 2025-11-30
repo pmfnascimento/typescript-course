52e82744fc25e8669ef84cd71f95ed8288adef78
Nascimento Pedro <pedro.nascimento@natixis.com>
2025-11-30 23:35:18 +0000

    Building a First Decorator

---
+++ 11-ecmascript-decorators/decorators.ts
@@ -0,0 +1,16 @@
+function logger(target: any, ctx: ClassDecoratorContext) {
+  console.log("logger decorator");
+  console.log(target);
+  console.log(ctx);
+}
+
+@logger
+class Person {
+  name = "Max";
+
+  greet() {
+    console.log("Hi, I am " + this.name);
+  }
+}
+
+let person = new Person();
