71313cd90348a1cb28abdb14896a60d9c836b9fe
Pedro Nascimento <pmfnascimento@gmail.com>
2025-11-30 14:35:01 +0000

    Interfaces As Object Types

--- 06-classes-interfaces/interfaces.ts
+++ 06-classes-interfaces/interfaces.ts
@@ -1,8 +1,20 @@
 interface Authenticatable {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 
-    login(): void;
-    logout(): void;
+  login(): void;
+  logout(): void;
 }
 
+let user: Authenticatable;
+
+user = {
+  email: "test@email.com",
+  password: "123",
+  login() {
+    console.log("Logout");
+  },
+  logout() {
+    console.log("Logout");
+  },
+};
