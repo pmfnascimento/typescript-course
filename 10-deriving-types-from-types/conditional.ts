7bb6d91096d20d34cab776ebe6724f2fabb05ca2
Nascimento Pedro <pedro.nascimento@natixis.com>
2025-11-30 22:55:09 +0000

    Conditional Types - Another Example

--- 10-deriving-types-from-types/conditional.ts
+++ 10-deriving-types-from-types/conditional.ts
@@ -12,6 +12,22 @@ let text = 1;
 type GetElementType<T> = T extends any[] ? T[number] : never;
 
 type Example1 = GetElementType<StringArray>;
-type Example2 = GetElementType<typeof text>
+type Example2 = GetElementType<typeof text>;
 
+type FullNamePerson = { firstName: string; lastName: string };
+type FullNameOrNothing<T> = T extends FullNamePerson ? string : never;
+function getFullName<T extends object>(person: T): FullNameOrNothing<T> {
+  if (
+    "firstName" in person &&
+    "lastName" in person &&
+    person.firstName &&
+    person.lastName
+  ) {
+    return `${person.firstName} ${person.lastName}` as FullNameOrNothing<T>;
+  }
 
+  throw new Error("Invalid person object");
+}
+
+const name1 = getFullName({});
+const name2 = getFullName({ firstname: "Max", lastName: "Muller" });
