f024238406c41d258d53765b277a83fb67afc5e7
Nascimento Pedro <pedro.nascimento@natixis.com>
2025-12-01 01:40:38 +0000

    Returning (and changing) a Class in a Class Decorator

--- 12-experimental-decorators/decorators-01-starting-setup/src/app.ts
+++ 12-experimental-decorators/decorators-01-starting-setup/src/app.ts
@@ -8,15 +8,23 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(template: string, hookId: string) {
-  return function (constructor: any) {
-    console.log("Rending templating");
+  console.log("TEMPLATE FACTORY");
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T
+  ) {
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
+        console.log("Rending templating");
 
-    const hookEl = document.getElementById(hookId);
-    const p = new constructor();
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      hookEl.querySelector("h1")!.textContent = p.name;
-    }
+        const hookEl = document.getElementById(hookId);
+        const p = new originalConstructor();
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector("h1")!.textContent = p.name;
+        }
+      }
+    };
   };
 }
 
