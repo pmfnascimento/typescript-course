84f65911fdbd79a4e56edea73084ff83c44ecdde
Nascimento Pedro <pedro.nascimento@natixis.com>
2025-12-01 00:45:06 +0000

    Building More Useful Decorators

--- 12-experimental-decorators/decorators-01-starting-setup/src/app.ts
+++ 12-experimental-decorators/decorators-01-starting-setup/src/app.ts
@@ -5,7 +5,18 @@ function Logger(logString: string) {
   };
 }
 
-@Logger("LOGGING - PERSON")
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
+@WithTemplate("<h1>Person Template</h1>", "app")
 class Person {
   name = "Max";
 
