44201b9ce45b9be5d92c914c1dabb8313232e699
Nascimento Pedro <pedro.nascimento@natixis.com>
2025-12-01 14:49:49 +0000

    Filtering Projects with Enums

--- 13-drag-drop-project/src/app.ts
+++ 13-drag-drop-project/src/app.ts
@@ -243,7 +243,13 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
 
     projectState.addListener((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter((prj) => {
+        if (this.type === "active") {
+          return prj.status === ProjectStatus.Active;
+        }
+        return prj.status === ProjectStatus.Finished;
+      });
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
 
@@ -255,6 +261,7 @@ class ProjectList {
     const list = document.getElementById(
       `${this.type}-projects-list`
     ) as HTMLTemplateElement;
+    list.innerHTML = "";
     for (const item of this.assignedProjects) {
       const listItem = document.createElement("li");
       listItem.textContent = item.title;
