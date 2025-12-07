87fe0550d2bc245a91d2c0b4fa97343bf8f948fe
Nascimento Pedro <pedro.nascimento@natixis.com>
2025-12-07 16:43:32 +0000

    Adding Another Components With Props

--- 19-reactjs-typescript/src/App.tsx
+++ 19-reactjs-typescript/src/App.tsx
@@ -1,4 +1,5 @@
 import Header from './components/Header'
+import CourseGoals from './components/CourseGoals'
 import goalsImg from './assets/goals.jpg'
 function App() {
 
@@ -7,6 +8,18 @@ function App() {
     <Header image={{src: goalsImg, alt: 'A list of goals'}}>
       <h1>Hello World Children!</h1>
     </Header>
+    <CourseGoals goals={[
+      {
+        id: 1, 
+        title: 'Learn TS', 
+        description: 'Learn TS from the groud up'
+      },
+      {
+        id: 2, 
+        title: 'Pratice TS', 
+        description: 'Pratice working with TypeScript'
+      }
+        ]} />
     </main>
   )
 }
---
+++ 19-reactjs-typescript/src/components/CourseGoals.tsx
@@ -0,0 +1,30 @@
+type Goal = {
+    id: number;
+    title: string;
+    description: string;
+}
+
+interface CourseGoalsProps {
+    goals: Goal[]
+}
+
+function CourseGoals({goals}: CourseGoalsProps) {
+
+    return (
+        <ul>
+            {goals.map((goal) => 
+                <li key={goal.id}>
+                    <article>
+                        <div>
+                            <h2>{goal.title}</h2>
+                            <p>{goal.description}</p>
+                        </div>
+                        <button>Delete</button>
+                    </article>
+                </li>
+            )}
+        </ul>
+    );
+}
+
+export default CourseGoals;
