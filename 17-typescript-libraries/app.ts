7e3bab0d317ccc6757ddde9e9c91afef50c291f2
Nascimento Pedro <pedro.nascimento@natixis.com>
2025-12-07 13:20:02 +0000

    Exploring Zod As a TypeScript-first Library

--- 17-typescript-libraries/app.ts
+++ 17-typescript-libraries/app.ts
@@ -1,7 +1,15 @@
-import _ from "lodash";
+// import _ from "lodash";
 
-const numbers = [1, 2, 3, 4, 5, 6];
+// const numbers = [1, 2, 3, 4, 5, 6];
 
-const chunkeddArr = _.chunk(numbers, 2);
+// const chunkeddArr = _.chunk(numbers, 2);
 
-console.log(chunkeddArr);
+// console.log(chunkeddArr);
+
+import { z } from "zod";
+import fs from "node:fs";
+
+const dataSchema = z.string();
+const content = fs.readFileSync("../data.json");
+
+const parseData = dataSchema.parse(content);
