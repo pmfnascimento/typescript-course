305daecef3352c68b327032f77ce63f8a183432f
Nascimento Pedro <pedro.nascimento@natixis.com>
2025-12-07 13:27:13 +0000

    Diving Deeper Into Zod

--- 17-typescript-libraries/app.ts
+++ 17-typescript-libraries/app.ts
@@ -9,7 +9,14 @@
 import { z } from "zod";
 import fs from "node:fs";
 
-const dataSchema = z.string();
-const content = fs.readFileSync("../data.json");
+const dataSchema = z.object({
+  title: z.string(),
+  id: z.number(),
+  values: z.array(z.union([z.string(), z.number()])),
+});
+
+const content = JSON.parse(fs.readFileSync("../data.json").toString());
 
 const parseData = dataSchema.parse(content);
+
+console.log(parseData);
